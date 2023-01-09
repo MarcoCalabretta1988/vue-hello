3d1d8c14ed5e81d82375cc2d10f49ff30a072bf2
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-01-09 14:30:32 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// CHECK JS LINK
+console.log('JS OK');
+console.log('VUE ok',Vue);
+
+//! TESTO ESERCIZIO
+
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+ */
