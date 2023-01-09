75418799cc6c25d1be24c9655407799a8d776482
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-01-09 14:57:35 +0100

    exercise and bonus

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
  */
+
+//? CREAZIONE APP DI VUE
+
+const app = Vue.createApp({
+    data(){
+        return{
+            message : 'Hello Vue!',
+            imgUrl : 'https://picsum.photos/seed/picsum/200/300',
+        }
+    },
+});
+
+app.mount('#root');
